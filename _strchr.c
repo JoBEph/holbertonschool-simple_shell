e4ed1a036325c14ef1a768042e751a29e98e958f
Julie Tolve <9919@holbertonstudents.com>
2024-12-18 09:41:11 +0100

    Function to locate a character

---
+++ _strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strchr - function which locate a character in a string
+ * @s: parameter pointer
+ * @c: character
+ * Return: char (pointer)
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s && *s != c)
+		s++;
+
+	if (*s == c)
+		return (s);
+	else
+		return ('\0');
+}
