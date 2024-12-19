dda0f18c7cb80d3a24553309e937d6a4e6e49031
root <root@JordanPro.localdomain>
2024-12-19 12:13:09 +0100

    add news librarys

--- main.h
+++ main.h
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
 /* Prototypes de function */
-
-char *_strchr(char *s, char c);
 
 
 #endif
