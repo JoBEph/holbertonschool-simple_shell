7d0aa2d999f925274f1f5d532d37f3aae3167bb5
root <root@JordanPro.localdomain>
2025-01-03 15:23:11 +0100

    execute cmd need review

--- execmd.c
+++ execmd.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
-void execmd(char **argv){
-    char *command = NULL;
+/**
+ *execmd - create new process with fork
+ *@argv: array of character poiters listing arguments
+ *Return: Void
+ */
 
-    if (argv){
-        /* get the command */
-        command = argv[0];
+void execmd(char **argv)
+{
+	pid_t pid = 0;
+	int status = 0;
 
-        /* execute the command with execve */
-        if (execve(command, argv, NULL) == -1){
-            perror("Error:");
-        };
-    }
-
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+	}
+	else if (pid > 0)
+	{
+		waitpid(pid, &status, 0);
+		kill(pid, SIGTERM);
+	}
+	else
+	{
+		if (execve(cmd[0], cmd, NULL) == -1)
+		{
+			perror("We found nothing");
+		}
+		exit(EXIT_FAILURE);
+	}
 }
-
