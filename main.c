55beb9bee0dcc06938cc34b7e8b6a01d16d5c46d
Julie Tolve <9919@holbertonstudents.com>
2025-01-08 15:28:48 +0100

    create main function and header file

---
+++ main.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * main - function to use a simple shell command and execute
+ * @argc: void (not use in this function)
+ * @argv: void (not use in this function)
+ * Return: Success of the execution (0)
+ */
+
+int main(int argc, char **argv)
+{
+	char *buffer = NULL, *path;
+	size_t n = 0;
+	ssize_t rline;
+	char **array;
+
+	(void)argc, (void)argv;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "C is not fun $ ", 15);
+			fflush(stdout);
+		}
+
+		rline = getline(&buffer, &n, stdin);
+
+		if (rline == -1)
+		{
+			free(buffer);
+			exit(0);
+		}
+		array = token_input(buffer);
+		if (array == NULL)
+		{
+			free(buffer);
+			continue;
+		}
+		path = get_file_path(array[0]);
+
+		if (path == NULL)
+		{
+			perror("Command not found");
+			free(buffer);
+			free(array);
+			free(path);
+			continue;
+		}
+		if (fork_exe(array, path) == -1)
+		{
+			free(buffer);
+			free(array);
+			free(path);
+			exit(1);
+		}
+		free(buffer);
+		free(array);
+		free(path);
+	}
+	free(buffer);
+	return (0);
+}
---
+++ main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+
+/* Prototypes de function */
+
+char *get_file_loc(char *PATH, char *file_name);
+char *get_file_path(char *file_name);
+int slash(const char *str);
+int main(int argc, char **argv);
+int fork_exe(char **array, char *path);
+char **token_input(char *buffer);
+
+#endif
