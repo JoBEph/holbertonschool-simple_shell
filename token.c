e6965117d85701fc3e841ac0020dd12d4a92bde8
Julie Tolve <9919@holbertonstudents.com>
2025-01-08 15:29:24 +0100

    create function to split input

---
+++ token.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * token - function tokenise input command
+ * @buffer: 
+ * Return: array
+ */
+
+char **token_input(char *buffer)
+{
+	char *token;
+	int i;
+	char **array;
+
+	token = strtok(buffer, " \n");
+
+	array = malloc(sizeof(char *) * 1024);
+	if (array == NULL)
+	{
+		perror("Memory allocation failed");
+		free(buffer);
+		exit(1);
+	}
+
+	i = 0;
+	while (token)
+	{
+		array[i] = token;
+		token = strtok(NULL, " \n");
+		i++;
+	}
+
+	array[i] = NULL;
+	return (array);
+}
