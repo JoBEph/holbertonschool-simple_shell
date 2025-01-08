1b153fa46cac2282c21883b3882947dc21da2d65
root <root@JordanPro.localdomain>
2025-01-08 10:36:36 +0100

    test update

--- main.c
+++ main.c
@@ -6,55 +6,56 @@
  * @argv: void (not use in this function)
  *
  * Return: Success of the execution (0)
-*/
+ */
 int main(int argc, char **argv)
 {
-    char *buffer = NULL, *path;
-    size_t n = 0;
-    ssize_t rline;
-    char **array;
+	char *buffer = NULL, *path;
+	size_t n = 0;
+	ssize_t rline;
+	char **array;
 
-    (void)argc, (void)argv;
+	(void)argc, (void)argv;
 
-    while (1)
-    {
-        if (isatty(STDIN_FILENO))
-        {
-            write(STDOUT_FILENO, "C is not fun $ ", 15);
-            fflush(stdout);
-        }
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			write(STDOUT_FILENO, "C is not fun $ ", 15);
+			fflush(stdout);
+		}
 
-        rline = getline(&buffer, &n, stdin);
+		rline = getline(&buffer, &n, stdin);
 
-        if (rline == -1)
-        {
-            exit(0);
-        }
-        array = tokenize_input(buffer);
+		if (rline == -1)
+		{
+			free(buffer);
+			exit(0);
+		}
+		array = tokenize_input(buffer);
 
-        if (array == NULL)
-        {
-            free(buffer);
-            continue;
-        }
+		if (array == NULL)
+		{
+			free(buffer);
+			continue;
+		}
 
-        path = get_file_path(array[0]);
+		path = get_file_path(array[0]);
 
-        if (path == NULL)
-        {
-            perror("Command not found");
-            free_resources(buffer, array, path);
-            continue;
-        }
-        if (fork_and_execute(array, path) == -1)
-        {
-            free_resources(buffer, array, path);
-            exit(1);
-        }
+		if (path == NULL)
+		{
+			perror("Command not found");
+			free_resources(buffer, array, path);
+			continue;
+		}
+		if (fork_and_execute(array, path) == -1)
+		{
+			free_resources(buffer, array, path);
+			exit(1);
+		}
 
-        free_resources(buffer, array, path);
-    }
+		free_resources(buffer, array, path);
+	}
 
-    free(buffer);
-    return (0);
+	free(buffer);
+	return (0);
 }
