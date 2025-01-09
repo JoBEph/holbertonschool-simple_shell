2d6e2cc7d0c8c432a2532ffbbced7a8a47978634
root <root@JordanPro.localdomain>
2025-01-09 13:33:05 +0100

    reduce the main file for betty

--- main.c
+++ main.c
@@ -42,17 +42,13 @@ int main(int argc, char **argv)
 		child_pid = fork();
 		if (child_pid == -1)
 		{	perror("Failed to create.");
-			free(array);
-			free(path);
-			free(buffer);
+			_free(buffer, array, path);
 			exit(41);	}
 		if (child_pid == 0)
 		{
 			if (execve(path, array, NULL) == -1)
 			{	perror("Failed to execute");
-				free(path);
-				free(array);
-				free(buffer);
+				_free(buffer, array, path);
 				exit(97);	}
 		}
 		else
