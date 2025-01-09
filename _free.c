081c0787b03e74e99878f4aad66fb4eb60f21e67
root <root@JordanPro.localdomain>
2025-01-09 13:30:48 +0100

    add free fonction for 3 buffers

---
+++ _free.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _free - Frees allocated memories.
+ * @buffer: The input buffer to free.
+ * @array: The array of tokens to free.
+ * @path: The path string to free.
+*/
+
+void _free(char *buffer, char **array, char *path)
+{
+	int i;
+
+	if (buffer != NULL)
+	{
+		free(buffer);
+	}
+
+	if (array != NULL)
+	{
+		for (i = 0; array[i] != NULL; i++)
+		{
+			free(array[i]);
+		}
+		free(array);
+	}
+
+	if (path != NULL)
+	{
+		free(path);
+	}
+}
