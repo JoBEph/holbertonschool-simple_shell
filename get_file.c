7b6f64cd93ea8564eb8d2f1041c21039854e0d2b
root <root@JordanPro.localdomain>
2025-01-09 23:03:58 +0100

    fix path

--- get_file.c
+++ get_file.c
@@ -21,14 +21,14 @@ int slash(const char *str)
 
 char *get_path(char *file_name)
 {
-	char *path_copy, *path, *token, *temp_path, *full_path = NULL;
+	char *path_copy, *token, *temp_path, *full_path = NULL;
 	size_t path_len, file_len, total_len;
+	char *path = _getenv("PATH");
 
 	if (!file_name || file_name[0] == '\0')
 		return (NULL);
 	if (strchr(file_name, '/') && access(file_name, X_OK) == 0)
 		return (strdup(file_name));
-	path = _getenv("PATH");
 	if (!path)
 	{	fprintf(stderr, "%s: PATH environnement variable not set\n", file_name);
 		return (NULL);
@@ -50,7 +50,7 @@ char *get_path(char *file_name)
 			free(full_path);
 			return (NULL);
 		}
-		snprintf(temp_path, total_len, "%s%s", token, file_name);
+		snprintf(temp_path, total_len, "%s/%s", token, file_name);
 		if (access(temp_path, X_OK) == 0)
 		{	free(path_copy);
 			return (temp_path);
