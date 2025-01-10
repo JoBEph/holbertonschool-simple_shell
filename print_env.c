bf2db23f45f23062991b4cd9dac562c5579dc12c
Julie Tolve <9919@holbertonstudents.com>
2025-01-10 11:05:54 +0100

    try to fix env

--- print_env.c
+++ print_env.c
@@ -3,13 +3,16 @@
 /**
  * print_env - Loops through all environment variables and prints them
  * Return: nothing (void)
-*/
+ */
 
 
 void print_env(void)
 {
-	char **env;
+	char **env = environ;
 
-	for (env = environ; *env != NULL; env++)
+	while (*env)
+	{
 		printf("%s\n", *env);
+		env++;
+	}
 }
