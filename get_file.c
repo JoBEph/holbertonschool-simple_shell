c10a79c3921d1f93d6609468ee516928b0329565
Julie Tolve <9919@holbertonstudents.com>
2025-01-10 12:37:17 +0100

    Remove 2 functions and create 1 function

--- get_file.c
+++ get_file.c
@@ -14,80 +14,51 @@ int slash(const char *str)
 }
 
 /**
- * get_file_loc - get the executable path of the file
- * @path: full path variable
+ * get_path - get the executable path of the file
  * @file_name: the executable file
  * Return: NULL
  */
 
-char *get_file_loc(char *path, char *file_name)
+char *get_path(char *file_name)
 {
-	char *path_copy = NULL, *token = NULL;
-	struct stat file_path;
-	char *path_buffer = NULL;
-	size_t path_len, file_len;
+	char *path_copy, *path, *token, *temp_path, *full_path = NULL;
+	size_t path_len, file_len, total_len;
 
-	if (!path || !file_name)
+	if (!file_name || file_name[0] == '\0')
 		return (NULL);
-
+	if (strchr(file_name, '/') && access(file_name, X_OK) == 0)
+		return (strdup(file_name));
+	path = _getenv("PATH");
+	if (!path)
+	{	fprintf(stderr, "%s: PATH environnement variable not set\n", file_name);
+		return (NULL);
+	}
+	file_len = strlen(file_name);
 	path_copy = strdup(path);
 	if (!path_copy)
-	{
-		perror("Error: strdup failed");
+	{	perror("strdup");
 		return (NULL);
 	}
 	token = strtok(path_copy, ":");
 	while (token)
-	{
-		path_len = strlen(token);
-		file_len = strlen(file_name);
-		path_buffer = realloc(path_buffer, path_len + file_len + 2);
-		if (!path_buffer)
-		{
-			perror("Error: realloc failed");
+	{	path_len = strlen(token);
+		total_len = path_len + file_len + 2;
+		temp_path = malloc(total_len);
+		if (!temp_path)
+		{	perror("malloc");
 			free(path_copy);
+			free(full_path);
 			return (NULL);
 		}
-		_strcpy(path_buffer, token);
-		path_buffer[path_len] = '/';
-		_strcpy(path_buffer + path_len + 1, file_name);
-		if (stat(path_buffer, &file_path) == 0 && access(path_buffer, X_OK) == 0)
-		{
-			free(path_copy);
-			return (path_buffer);
+		snprintf(temp_path, total_len, "%s%s", token, file_name);
+		if (access(temp_path, X_OK) == 0)
+		{	free(path_copy);
+			return (temp_path);
 		}
+		free(temp_path);
 		token = strtok(NULL, ":");
 	}
 	free(path_copy);
-	free(path_buffer);
+	fprintf(stderr, "%s: command not found\n", file_name);
 	return (NULL);
-}
-/**
- * get_file_path - get the full path of the file
- * @file_name: Argument name
- * Return: the fulle path argument to the file
- */
-
-char *get_file_path(char *file_name)
-{
-	char *path = _getenv("PATH");
-	char *full_path;
-
-	if (slash(file_name) && access(file_name, X_OK) == 0)
-		return (strdup(file_name));
-
-	if (!path)
-	{
-		perror("Path not found");
-		return (NULL);
-	}
-	full_path = get_file_loc(path, file_name);
-
-	if (full_path == NULL)
-	{
-		write(2, file_name, strlen(file_name));
-		write(2, ": command not found\n", 19);
-		return (NULL);
-	}
-	return (full_path);
 }
